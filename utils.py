235f45358f6897984c1532e6fba0b189bbbc1141
Jing Cao <jcao0715@gmail.com>
2025-12-08 03:17:36 -0500

    comments

--- utils.py
+++ utils.py
@@ -2,11 +2,17 @@ import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 def khatri_rao(P, Q):
+    """
+    Khatri-Rao product (block Kronecker product)
+    """
     d1, k = P.shape
     d2, _ = Q.shape
     return np.hstack([np.kron(P[:, i], Q[:, i])[:, np.newaxis] for i in range(k)])
 
 def reconstruct_tensor(A, B, C):
+    """
+    Reconstruct tensor from factor matrices via CP decomposition.
+    """
     d, k = A.shape
     T = np.zeros((d, d, d))
     for i in range(k):
@@ -14,12 +20,18 @@ def reconstruct_tensor(A, B, C):
     return T
 
 def fro_norm(X):
+    """
+    Frobenius norm
+    """
     return np.sqrt(np.sum(X**2))
 
 def reconstruction_error(true_T, hat_T):
     return fro_norm(true_T - hat_T) / fro_norm(true_T)
 
 def factor_recovery_error(true_F, hat_F):
+    """
+    Factor recovery error by matching the factor matrices up to a rotation.
+    """
     d, k = true_F.shape
     true_norm = true_F / np.linalg.norm(true_F, axis=0)
     hat_norm = hat_F / np.linalg.norm(hat_F, axis=0)
